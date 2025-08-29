ca0d92cbc60dd7323b48ef1a95ad74a8de21f9ef
wilson-rimac <wilson.julca@rimac.com.pe>
2025-08-29 05:10:57 -0500

    update server

--- server.mjs
+++ server.mjs
@@ -1,45 +1,12 @@
-import * as LaunchDarkly from "@launchdarkly/node-server-sdk";
 import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 const port = 3000;
 
-const client = LaunchDarkly.init(process.env.LD_SDK_KEY);
+app.get('/', (req, res) => {
+  res.send('Welcome to my server!');
+});
 
-const context = {
-  kind: "user",
-  key: "user-key-123abcde",
-  email: "biz@face.dev",
-};
-
-client.once("ready", function () {
-  console.log("SDK successfully initialized!");
-
-  app.get("/", async (req, res) => {
-    // Tracking your memberId lets us know you are connected.
-    client.track(process.env.LD_EVENT_KEY, context);
-
-    // Check feature flag value
-    client.variation(
-      "feat-new-menu",
-      context,
-      false,
-      function (err, showFeature) {
-        if (showFeature) {
-          console.log("feature true");
-          res.send("Feature flag is ON");
-        } else {
-          console.log("feature false");
-          res.send("Feature flag is OFF");
-        }
-      }
-    );
-  });
-
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
